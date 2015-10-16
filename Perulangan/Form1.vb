aa43bdc1e87fe0fe8021b3bcbe363b5051dcb9ea
Riski Anugrah Putra <riskicos@gmail.com>
2015-10-16 10:59:34 +0700

    TUGAS PERULANGAN
    Riski Anugrah Putra/MI-2E/1431140009

--- Perulangan/Form1.vb
+++ Perulangan/Form1.vb
@@ -1,3 +1,24 @@
 ﻿Public Class Form1
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub ProblemBintangToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ProblemBintangToolStripMenuItem.Click
+        Form2.MdiParent = Me
+        Form2.Show()
+    End Sub
+
+    Private Sub MatriksKalkulatorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles MatriksKalkulatorToolStripMenuItem.Click
+        Form3.MdiParent = Me
+        Form3.Show()
+    End Sub
+
+    Private Sub KeluarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles KeluarToolStripMenuItem.Click
+        Dim konfirmasi As DialogResult = MessageBox.Show("Anda yakin ingin keluar?", "Konfirmasi", MessageBoxButtons.YesNo)
+        If konfirmasi = Windows.Forms.DialogResult.Yes Then
+            Environment.Exit(0)
+        Else
+        End If
+    End Sub
 End Class
--- Perulangan/Form2.vb
+++ Perulangan/Form2.vb
@@ -1,4 +1,107 @@
 ﻿Public Class Form2
+  
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-    
+        Select Case ComboBox1.Text
+            Case "Pyramid"
+                pyramid()
+            Case "Hollow Pyramid"
+                hollowpyramid()
+            Case "Inverted Pyramid"
+                invertedpyramid()
+            Case "Hollow Inverted Pyramid"
+                hollowinvertedpyramid()
+            Case Else
+                TextBox1.Text = "Pilih pola yang anda inginkan"
+        End Select
+    End Sub
+    Private Sub pyramid()
+        Dim Cetak As String = ""
+        Dim batas As Integer = TextBox1.Text
+        Dim batasKolom1 As Integer = batas
+        Dim batasKolom2 As Integer = batas
+        For baris As Integer = 1 To batas
+            For kolom As Integer = 1 To batas * 2 - 1
+                If (kolom < batasKolom1 Or kolom > batasKolom2) Then
+                    Cetak &= "  "
+                Else
+                    Cetak &= " *"
+                End If
+            Next
+            batasKolom1 -= 1
+            batasKolom2 += 1
+            Cetak &= vbCrLf
+        Next
+        TextBox2.Text = Cetak
+        Cetak = ""
+    End Sub
+    Private Sub hollowpyramid()
+        Dim cetak As String = ""
+        Dim batas As Integer = TextBox1.Text
+        Dim batasKolom1 As Integer = batas
+        Dim batasKolom2 As Integer = batas
+        For baris As Integer = 1 To batas
+            For kolom As Integer = 1 To batas * 2 - 1
+                If (baris < batas) Then
+                    If (kolom = batasKolom1 Or kolom = batasKolom2) Then
+                        cetak &= " *"
+                    Else
+                        cetak &= "  "
+                    End If
+                Else
+                    cetak &= " *"
+                End If
+            Next
+            batasKolom1 -= 1
+            batasKolom2 += 1
+            cetak &= vbCrLf
+        Next
+        TextBox2.Text = cetak
+        cetak = ""
+    End Sub
+    Private Sub invertedpyramid()
+        Dim cetak As String = ""
+        Dim batas As Integer = TextBox1.Text
+        Dim batasKolom1 As Integer = 0
+        Dim batasKolom2 As Integer = batas * 2
+        For baris As Integer = 1 To batas
+            For kolom As Integer = 1 To batas * 2 - 1
+                If (kolom > batasKolom1 And kolom < batasKolom2) Then
+                    cetak &= " *"
+                Else
+                    cetak &= "  "
+                End If
+            Next
+            cetak &= vbCrLf
+            batasKolom1 += 1
+            batasKolom2 -= 1
+        Next
+        TextBox2.Text = cetak
+        cetak = ""
+    End Sub
+    Private Sub hollowinvertedpyramid()
+        Dim cetak As String = ""
+        Dim batas As Integer = TextBox1.Text
+        Dim batasKolom1 As Integer = 1
+        Dim batasKolom2 As Integer = batas * 2 - 1
+        For baris As Integer = 1 To batas
+            For kolom As Integer = 1 To batas * 2 - 1
+                If (baris = 1) Then
+                    cetak &= " *"
+                Else
+                    If (kolom = batasKolom1 Or kolom = batasKolom2) Then
+                        cetak &= " *"
+                    Else
+                        cetak &= "  "
+                    End If
+                End If
+            Next
+            batasKolom1 += 1
+            batasKolom2 -= 1
+            cetak &= vbCrLf
+        Next
+        TextBox2.Text = cetak
+        cetak = ""
+    End Sub
+
 End Class
